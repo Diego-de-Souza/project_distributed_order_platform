47f5f7770e2da9e8b4b4abc655ece1e49aa24203
Diego-de-Souza <diegodesouza.souza@gmail.com>
2026-09-01 11:11:34 -0300

    fix: correcao da abertura e fechamento de instancias que gera erro ao adicionar mais condicoes de teste

--- api_nestjs_distributed_order_platform/test/app.e2e-spec.ts
+++ api_nestjs_distributed_order_platform/test/app.e2e-spec.ts
@@ -7,7 +7,7 @@ import { AppModule } from './../src/app.module';
 describe('health check', () => {
   let app: INestApplication<App>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
